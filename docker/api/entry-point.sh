634b7e7346c709fad0758e5efc37d4040a6a5ed2
Marc-Antoine DUMAR <marcantoine.dumar@gmail.com>
2026-02-13 15:42:33 +0100

    déplacement de config dans entrypoint

--- docker/api/entry-point.sh
+++ docker/api/entry-point.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+cat <<EOF > /root/.ssh/config
+Host borg
+    IdentityFile ~/.ssh/id_ed25519
+EOF
 chmod 600 /root/.ssh/id_ed25519
 chown root:root /root/.ssh/id_ed25519
 /usr/local/sbin/strongholer
