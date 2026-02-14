99f012c69fec288dccda7f45ddf517c8d0cf8973
Marc-Antoine DUMAR <marcantoine.dumar@gmail.com>
2026-02-14 19:54:49 +0100

    passage de la clé en json et plus en fichier et externalisation de install_client_key

--- docker/api/src/route/send_ssh_key.rs
+++ docker/api/src/route/send_ssh_key.rs
@@ -1,14 +1,19 @@
 use actix_web::{post, HttpResponse, HttpRequest, web};
-use actix_multipart::Multipart;
-use futures_util::StreamExt as _;
+use openssh_sftp_client::file;
 use crate::authentification::auth::Auth;
 use crate::borg_script::install_client_key::install_client_key;
 const CLIENT_DIRECTORY: &str = "/srv/repos"; 
-use tokio::{process::Command, fs, io::AsyncWriteExt};
 const MAX_FILE_SIZE_SSH_KEY: usize = 50 * 1024 * 1024;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ssh_key{
+    ssh: String
+}
+
 
 #[post("send_ssh_key")]
-async fn send_ssh_key(req: HttpRequest, mut payload: Multipart, auth: web::Data<Auth>)->HttpResponse{
+async fn send_ssh_key(req: HttpRequest, ssh_key: web::Json<ssh_key>, auth: web::Data<Auth>)->HttpResponse{
     /* Extraction du cookie JWT */
     let Some(cookie) = req.cookie("Bearer") else{
         return HttpResponse::Ok().body("Pas de cookie Bearer")
@@ -18,32 +23,8 @@ async fn send_ssh_key(req: HttpRequest, mut payload: Multipart, auth: web::Data<
     let Some(credentials) = credentials else {
         return HttpResponse::Ok().body("Pas de credentials")
     };
-    
     /* Upload du fichier */
     let filepath= format!("/srv/repos/api/{}.pub", credentials.id,);
-    while let Some(field) = payload.next().await {
-        let mut field = field.expect("field invalide");
-        
-        // Crée le fichier
-        let mut f = auth.sftp_connexion.create(filepath.clone())
-            .await.expect("Création du fichier à échouer");
-
-        // Stream vers disque en chunks, sans charger en RAM
-        let mut written: usize = 0;
-        while let Some(chunk) = field.next().await {
-            let chunk = chunk.expect("chunk incorrect");
-            written = written.saturating_add(chunk.len());
-            if written > MAX_FILE_SIZE_SSH_KEY {
-                // Nettoyage si dépassement
-                let _ = auth.ssh_connexion.command("rm").arg(filepath).output().await;
-                return HttpResponse::Ok().body("fichier tros gros");
-            }
-            f.write_all(&chunk)
-                .await.expect("impossible d'écrire dans le fichier");
-        }
-    }
-
-    /* Execution du script d'ajout de la clé ssh */
-    install_client_key(credentials.id, filepath, auth.ssh_connexion.clone()).await;
+    install_client_key(credentials.id, &ssh_key.ssh, filepath, auth.ssh_connexion.clone(), auth.sftp_connexion.clone()).await;
     return HttpResponse::Ok().finish()
 }
