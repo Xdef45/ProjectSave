bb1c1580c25bcc35e8acef37430858ef2d89f498
TheStranger97 <bredassamuel@gmail.com>
2026-02-16 09:36:07 +0100

    added permission change before making actions on repo so the user can use his key, the scripts will automatically remove the key upon completion

--- docker/borg/server_scripts/restore.sh
+++ docker/borg/server_scripts/restore.sh
@@ -10,11 +10,20 @@ TARGET="${3-}"
 REPOSITORY_PATH="/srv/repos/${CLIENT}/repo/"
 RESTORE_PATH="/srv/repos/${CLIENT}/restore"
 
+KEY_CLEAR="${HOME_DIR}/.config/borg/keys/srv_repos_${CLIENT}_repo"
+API_USER="api"
+
+chmod 644 "${KEY_CLEAR}"
+chown ${CLIENT}:"${API_USER}" "${KEY_CLEAR}"
+
 if [ -z $TARGET ]; then
     sudo -u "${CLIENT}" borg export-tar "${REPOSITORY_PATH}"::"${ARCHIVE}" "${RESTORE_PATH}"/"${ARCHIVE}".tar.gz
 else
     cd $RESTORE_PATH
     sudo -u "${CLIENT}" borg extract "${REPOSITORY_PATH}"::"${ARCHIVE}" "${TARGET}"
 fi
+
+shred "${KEY_CLEAR}"
+rm "${KEY_CLEAR}"
 
 # option pour restore un fichier seul
